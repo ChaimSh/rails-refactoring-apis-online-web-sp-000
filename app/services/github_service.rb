47f121771ad6115da31fc16c91c0531b728fc14f
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-21 01:08:43 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -13,10 +13,6 @@ class GithubService
      response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: client_id, client_secret: client_secret ,code: code}, {'Accept' => 'application/json'}
      access_hash = JSON.parse(response.body)
      @access_token = access_hash["access_token"]
-
-     user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-     user_json = JSON.parse(user_response.body)
-     session[:username] = user_json["login"]
    end
 
 
