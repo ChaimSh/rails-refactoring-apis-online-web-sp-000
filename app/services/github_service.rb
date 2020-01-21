6aeea0a29ebe140b2cc501c3d7ffa42744252545
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-21 01:03:14 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,7 +1,7 @@
 class GithubService
 
-   def initialize(access_hash)
-    @access_hash = access_hash
+   def initialize(access_hash = nil)
+     @access_token = access_hash["access_token"] if access_hash
    end
 
 
